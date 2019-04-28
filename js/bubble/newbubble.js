5f175336c727755e525bab8083412c6c4c848a56
melvinabraham <melvin.abraham1996@gmail.com>
2019-04-27 18:31:54 -0700

    Added sentiment based colors and intensity

--- js/bubble/newbubble.js
+++ js/bubble/newbubble.js
@@ -1,16 +1,16 @@
 
+const urlParams = new URLSearchParams(window.location.search);
+const year = urlParams.get('year');
+const feat = urlParams.get('feat');
+
 function bubble() {
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const year = urlParams.get('year');	
-    const feat = urlParams.get('feat');	
 
     console.log(year)
     console.log(feat)
     file = "data\\year_features_5.json";
     d3.json(file).then(function (dataset) {
 
-        console.log("here")
         slide();
         var width = window.innerWidth, height = window.innerHeight;
         var svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
@@ -19,10 +19,17 @@ function bubble() {
             .size([width, height])
             .padding(1.5);
 
-        classes = drawchart(dataset, 2004)
-        redraw(classes)
+        // Build a dictionary of sentiments
+        const sentimentdata = dataset["__overall"]["opinion_sentiments"]
 
-        function redraw(classes) {
+        // Build a dictionary for opinionwords
+        opinions = createOpinionDict(dataset);
+
+
+        classes = drawchart(dataset, 2004)
+        redraw(classes, year)
+
+        function redraw(classes, curyear) {
 
             // transition
             var t = d3.transition()
@@ -41,7 +48,7 @@ function bubble() {
 
             //EXIT
             circle.exit()
-                .style("fill", "#b26745")
+            .attr("r", "white")
                 .transition(t)
                 .attr("r", 1e-6)
                 .remove();
@@ -54,7 +61,26 @@ function bubble() {
             //UPDATE
             circle
                 .transition(t)
-                .style("fill", "#add8e6")
+                .style("fill", function (d) {
+                    circle_name = d.data.name;
+                    hslvalue = 0.0;
+                    val = opinions[d.data.name];
+
+                    if(curyear in val) {
+                        hslvalue = scaleBetween(val[curyear], -0.8, 0.8, 1, 10);
+                        // console.log("data " + d.data.name + "Sentiment: "+ sentimentdata[circle_name] + "Year:  " + val[curyear] + "HSL: " +  hslvalue); 
+                    }
+
+                    if (sentimentdata[circle_name] > 0.0) {
+                        return ColorLuminance("#00FF7F", -hslvalue);;
+                    }
+                    else if (sentimentdata[circle_name] < 0) {
+                        return ColorLuminance("#fb8072", -hslvalue)
+                    }
+                    else {
+                        return "#33C3FF";
+                    }
+                })
                 .attr("r", function (d) { return d.r })
                 .attr("cx", function (d) { return d.x; })
                 .attr("cy", function (d) { return d.y; })
@@ -71,8 +97,25 @@ function bubble() {
                 .attr("cy", function (d) { return d.y; })
                 .style("fill", "#fff")
                 .transition(t)
-                .style("fill", "#45b29d")
+                .style("fill", function (d) {
+                    circle_name = d.data.name;
+                    hslvalue = 0.0;
+                    val = opinions[d.data.name];
 
+                    if(curyear in val) {
+                        hslvalue = scaleBetween(val[curyear], -0.6, 0.6, 1, 10);
+                    }
+
+                    if (sentimentdata[circle_name] > 0) {
+                        return ColorLuminance("#00FF7F", -hslvalue);;
+                    }
+                    else if (sentimentdata[circle_name] < 0) {
+                        return ColorLuminance("#fb8072", -hslvalue)
+                    }
+                    else {
+                        return "#33C3FF";
+                    }
+                })
                 .attr("r", function (d) { return d.r });
 
             text.enter().append("text")
@@ -94,7 +137,6 @@ function bubble() {
         function drawchart(dataset, year) {
             data = []
             d = dataset[year][feat]["opinions"];
-            console.log(d)
             for (var ele in d) {
                 data.push({ "name": ele, "size": d[ele] });
             }
@@ -125,12 +167,11 @@ function bubble() {
                 .default(new Date(value, 10, 3))
                 .on('onchange', val => {
                     cur = d3.timeFormat('%Y')(val)
-                    d3.select('p#value-time').text( feat.charAt(0).toUpperCase() + feat.slice(1) + " in " + value);
+                    d3.select('p#value-time').text(feat.charAt(0).toUpperCase() + feat.slice(1) + " in " + value);
                     if (cur != value) {
                         value = cur
                         classes = drawchart(dataset, cur);
-                        redraw(classes)
-                        // bubble(cur);
+                        redraw(classes, value)
                     }
 
                 });
@@ -155,4 +196,58 @@ function bubble() {
 }
 
 
+function ColorLuminance(hex, lum) {
 
+    // validate hex string
+    hex = String(hex).replace(/[^0-9a-f]/gi, '');
+    if (hex.length < 6) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    lum = lum || 0;
+
+    // convert to decimal and change luminosity
+    var rgb = "#", c, i;
+    for (i = 0; i < 3; i++) {
+        c = parseInt(hex.substr(i * 2, 2), 16);
+        c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+        rgb += ("00" + c).substr(c.length);
+    }
+
+    return rgb;
+}
+
+
+
+function createOpinionDict(dataset) {
+
+
+    count = 0;
+    opinionwords = {}
+
+
+    for (var year = 2004; year <= 2014; ++year) {
+
+        yearwiseopinions = dataset[year][feat]["opinions"]
+
+        for (word in yearwiseopinions) {
+
+            if (!(word in opinionwords)) {
+                opinionwords[word] = {};
+                opinionwords[word][year] = 1;
+                opinionwords[word]["max"] = 1;
+            }
+
+            else {
+                opinionwords[word][year] = opinionwords[word]["max"] + 1;
+                opinionwords[word]["max"] = opinionwords[word][year];
+            }
+        }
+    }
+
+    return opinionwords;
+
+}
+
+function scaleBetween(unscaledNum, minAllowed, maxAllowed, min, max) {
+    return (maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed;
+}
